e88209902157906f5756b221a43314bfedda8ae7
dylanagreen <dylanag@uci.edu>
2025-09-12 16:44:11 -0700

    Custom update_mtl function that uses custom target bits.

--- src/simassign/mtl.py
+++ src/simassign/mtl.py
@@ -6,38 +6,80 @@ import numpy as np
 from astropy.table import Table, join, vstack
 
 # DESI imports
-from desitarget.mtl import make_mtl
+from desitarget.mtl import make_mtl, get_utc_date
 
+# stdlib imports
+from importlib import resources
+import yaml
 
-def update_mtl(mtl, tids_to_update, rng=np.random.default_rng(91701)):
-    # By default we will use a dummy zcat for now to update.
-    # TODO write custom update that doesn't use zcats or desitarget so we
-    # can accurately handle a new target type.
-    unique_mtl = mtl[::-1]
-    _, ii = np.unique(unique_mtl["TARGETID"], return_index=True)
-    unique_mtl = unique_mtl[ii]
+def update_mtl(mtl, tids_to_update, use_desitarget=False):
+    # Use desitarget = use a dummy zcat and use make_mtl to update the MTL
+    if use_desitarget:
+        # If we use desitarget we will use a dummy zcat for now to update.
+        rng = np.random.default_rng() # Just used for dummy redshifts.
+        unique_mtl = mtl[::-1]
+        _, ii = np.unique(unique_mtl["TARGETID"], return_index=True)
+        unique_mtl = unique_mtl[ii]
 
-    # Make a dummy zcat to use for updating, which will be done using the original make_mtl function.
-    # dummy z_cat for update needs to have following cols:
-    # Z, ZWARN, ZTILEID, TARGETID and NUMOBS
-    # Z_QN, IS_QSO_QN, DELTACHI2
-    zcat = Table()
-    zcat["TARGETID"] = tids_to_update
-    zcat["Z"] = rng.uniform(2.2, 3.2, len(tids_to_update)) # they're all lya qsos, to ensure reobservation
-    zcat["ZWARN"] = np.zeros(len(tids_to_update), dtype=int) # They're all good redshifts
-    zcat["ZTILEID"] = np.ones(len(tids_to_update), dtype=int) # Arbitrary tile number
-    zcat["DELTACHI2"] = 50 * np.ones(len(tids_to_update), dtype=int)
-    zcat["IS_QSO_QN"] = np.ones(len(tids_to_update), dtype=bool)
-    zcat["Z_QN"] = zcat["Z"] + rng.uniform(-0.1, 0.1, len(tids_to_update))
+        # Make a dummy zcat to use for updating, which will be done using the original make_mtl function.
+        # dummy z_cat for update needs to have following cols:
+        # Z, ZWARN, ZTILEID, TARGETID and NUMOBS
+        # Z_QN, IS_QSO_QN, DELTACHI2
+        zcat = Table()
+        zcat["TARGETID"] = tids_to_update
+        zcat["Z"] = rng.uniform(2.2, 3.2, len(tids_to_update)) # they're all lya qsos, to ensure reobservation
+        zcat["ZWARN"] = np.zeros(len(tids_to_update), dtype=int) # They're all good redshifts
+        zcat["ZTILEID"] = np.ones(len(tids_to_update), dtype=int) # Arbitrary tile number
+        zcat["DELTACHI2"] = 50 * np.ones(len(tids_to_update), dtype=int)
+        zcat["IS_QSO_QN"] = np.ones(len(tids_to_update), dtype=bool)
+        zcat["Z_QN"] = zcat["Z"] + rng.uniform(-0.1, 0.1, len(tids_to_update))
 
-    # Increment the observations by 1, match the previous MTL to determine the current
-    # number of observations to get incremented.
-    zcat = join(unique_mtl["TARGETID", "NUMOBS"], zcat)
-    zcat["NUMOBS"] += 1
+        # Increment the observations by 1, match the previous MTL to determine the current
+        # number of observations to get incremented.
+        zcat = join(unique_mtl["TARGETID", "NUMOBS"], zcat)
+        zcat["NUMOBS"] += 1
 
-    # To update we need to compare against the deduplicated MTL to ensure we use the latest
-    # details for each targetid
-    mtl_updates = make_mtl(unique_mtl, "DARK", zcat=zcat, trimtozcat=True)
+        # To update we need to compare against the deduplicated MTL to ensure we use the latest
+        # details for each targetid
+        mtl_updates = make_mtl(unique_mtl, "DARK", zcat=zcat, trimtozcat=True)
+    else:
+        update_rows = np.isin(mtl["TARGETID"], tids_to_update)
+        mtl_updates = mtl[update_rows]
+
+        # Iterate the number of observations
+        mtl_updates["NUMOBS"] += 1
+
+        # Need to handle if we reovserved this extra times and don't want
+        # num obs more to go negative
+        nobs_more = mtl_updates["NUMOBS_MORE"]
+        mtl_updates["NUMOBS_MORE"] = np.where(nobs_more > 0, nobs_more - 1, 0)
+
+        targetmask = load_target_yaml("targetmask.yaml")
+
+        was_unobs = mtl_updates["NUMOBS"] == 1
+        is_complete = mtl_updates["NUMOBS_MORE"] == 0
+
+        for target in targetmask["desi_mask"]:
+            bit = 2**target[1]
+            name = target[0]
+
+            this_target = (mtl_updates["DESI_TARGET"] & bit) != 0
+
+            print(f"update loop target {target} {np.any(this_target)} {np.any(was_unobs & this_target)}")
+
+            # Set priority for done targets.
+            mtl_updates["PRIORITY"][this_target & is_complete] = targetmask["priorities"]["desi_mask"][name]["DONE"]
+            mtl_updates["TARGET_STATE"][this_target & is_complete] = "{name}|DONE"
+
+            # lazily assume that the target class is correct and we want more zgood.
+            mtl_updates["PRIORITY"][this_target & was_unobs] = targetmask["priorities"]["desi_mask"][name]["MORE_ZGOOD"]
+            mtl_updates["TARGET_STATE"][this_target & was_unobs] = "{name}|MORE_ZGOOD"
+
+        # This is important for storing the history of the MTL
+        mtl_updates["TIMESTAMP"] = get_utc_date("main")
+
+        # We won't update the redshift or anything, it's not really relevant to
+        # fiberassign anyway.
 
     # Append the updates to the full MTL, then sort it by TARGETID and TIMESTAMP
     # to put the updates in the right place.
@@ -70,3 +112,9 @@ def deduplicate_mtl(mtl):
     _, ii = np.unique(trunc_mtl["TARGETID"], return_index=True)
     trunc_mtl = trunc_mtl[ii]
     return trunc_mtl
+
+# TODO move this.
+def load_target_yaml(fname):
+    floc = resources.files("simassign").joinpath(f"data/{fname}")
+    with open(floc) as f:
+        return yaml.safe_load(f)
