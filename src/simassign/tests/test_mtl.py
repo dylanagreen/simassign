bf0c5a22342df96b84c679ef34a94e513163c87b
dylanagreen <dylanag@uci.edu>
2025-09-19 13:27:13 -0700

    Add unittests for updating MTL.

--- src/simassign/tests/test_mtl.py
+++ src/simassign/tests/test_mtl.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 
 from astropy.table import Table
@@ -10,9 +11,9 @@ class TestMTL(unittest.TestCase):
         tbl = Table()
         # Could use random objects here but we want this to be reproducible,
         # and for this test the location of the objects is entirely irrelevant.
-        nobj = 50
-        tbl["RA"] = np.linspace(110, 120, nobj)
-        tbl["DEC"] = np.linspace(10, 20, nobj)
+        n_obj = 50
+        tbl["RA"] = np.linspace(110, 120, n_obj)
+        tbl["DEC"] = np.linspace(10, 20, n_obj)
 
         observed_mtl = initialize_mtl(tbl)
 
@@ -26,16 +27,56 @@ class TestMTL(unittest.TestCase):
         # Using sets because the order does not matter
         self.assertEqual(set(observed_mtl.colnames), set(expected_colnames))
 
-        self.assertEqual(list(observed_mtl["DESI_TARGET"]), [2**2] * nobj)
-        self.assertEqual(list(observed_mtl["MWS_TARGET"]), [0] * nobj)
-        self.assertEqual(list(observed_mtl["BGS_TARGET"]), [0] * nobj)
 
-        self.assertEqual(list(observed_mtl["PRIORITY"]), [3400] * nobj)
+        np.testing.assert_array_equal(observed_mtl["DESI_TARGET"], 2**2 * np.ones(n_obj, dtype=int))
+        np.testing.assert_array_equal(observed_mtl["MWS_TARGET"], np.zeros(n_obj, dtype=int))
+        np.testing.assert_array_equal(observed_mtl["BGS_TARGET"], np.zeros(n_obj, dtype=int))
 
-        self.assertEqual(list(observed_mtl["NUMOBS"]), [0] * nobj)
-        self.assertEqual(list(observed_mtl["NUMOBS_INIT"]), [4] * nobj)
-        self.assertEqual(list(observed_mtl["NUMOBS_MORE"]), [4] * nobj)
+        # TODO no hardcoded - load from targetmask yaml.
+        np.testing.assert_array_equal(observed_mtl["PRIORITY"], 3400 * np.ones(n_obj, dtype=int))
+
+        np.testing.assert_array_equal(observed_mtl["NUMOBS"], np.zeros(n_obj, dtype=int))
+        np.testing.assert_array_equal(observed_mtl["NUMOBS_INIT"], 4 * np.ones(n_obj, dtype=int))
+        np.testing.assert_array_equal(observed_mtl["NUMOBS_MORE"], 4 * np.ones(n_obj, dtype=int))
 
         # Subpriority is random but we should ensure that it's between zero and one.
         self.assertTrue(np.all(observed_mtl["SUBPRIORITY"] >= 0))
         self.assertTrue(np.all(observed_mtl["SUBPRIORITY"] <= 1))
+
+    def test_update_mtl_nozcat(self):
+        tbl = Table()
+        n_obj = 50
+        n_update = 20
+        tbl["RA"] = np.linspace(110, 120, n_obj)
+        tbl["DEC"] = np.linspace(10, 20, n_obj)
+
+        init_mtl = initialize_mtl(tbl)
+        tids_to_update = observed_mtl["TARGETID"][0:n_update]
+        time.sleep(1) # Need the timestamps to differ, this code is too fast otherwise.
+        observed_mtl = update_mtl(init_mtl, tids_to_update=tids_to_update, use_desitarget=False)
+
+        # There should be at least 50 + 20 rows (50 original, 20 updated)
+        self.assertEqual(len(observed_mtl), n_obj + n_update)
+
+        for tid in tids_to_update:
+            # Indices in this loop based on the assumption that update_mtl
+            # correctly orders the mtl by targetid and then by timestamp
+            # so that the later row is a later timestamp
+            tid_tbl = observed_mtl[observed_mtl["TARGETID"] == tid]
+            # Every updated targetid should have two rows in the new MTL.
+            self.assertEqual(len(tid_tbl), 2)
+
+            # NUMOBS should be increased by 1 in the updated row.
+            # Should be equal to zero in the original row
+            self.assertEqual(tid_tbl["NUMOBS"][0] + 1, tid_tbl["NUMOBS"][1])
+            self.assertEqual(tid_tbl["NUMOBS"][0], 0)
+
+            # NUMOBS_MORE should be decreased by one relative to previous.
+            # Original should match the targetmask yaml.
+            self.assertEqual(tid_tbl["NUMOBS_MORE"][0] - 1, tid_tbl["NUMOBS_MORE"][1])
+            self.assertEqual(tid_tbl["NUMOBS_MORE"][0], 4) # TODO not hardcoded
+
+            # The priority both before and after the update should match
+            # what's expected in our custom targetmask.
+
+            # TODO more tests...
