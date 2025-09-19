d27e9d56e27d71e598fe375aa31cf296a4d33fd2
dylanagreen <dylanag@uci.edu>
2025-09-19 13:52:15 -0700

    Unhardcode some tests and load from targetmask.yaml instead.

--- src/simassign/tests/test_mtl.py
+++ src/simassign/tests/test_mtl.py
@@ -7,6 +7,10 @@ import numpy as np
 from simassign.mtl import *
 
 class TestMTL(unittest.TestCase):
+    def setUp(self):
+        # Need this for checking priorities etc.
+        self.targetmask = load_target_yaml("targetmask.yaml")
+
     def test_initialize_mtl(self):
         tbl = Table()
         # Could use random objects here but we want this to be reproducible,
@@ -27,23 +31,32 @@ class TestMTL(unittest.TestCase):
         # Using sets because the order does not matter
         self.assertEqual(set(observed_mtl.colnames), set(expected_colnames))
 
+        # TODO a better way to do this bit detecting?
+        true_bit = self.targetmask["desi_mask"][0][1]
+        np.testing.assert_array_equal(observed_mtl["DESI_TARGET"],
+                                      2**true_bit * np.ones(n_obj, dtype=int))
+        np.testing.assert_array_equal(observed_mtl["MWS_TARGET"],
+                                      np.zeros(n_obj, dtype=int))
+        np.testing.assert_array_equal(observed_mtl["BGS_TARGET"],
+                                      np.zeros(n_obj, dtype=int))
 
-        np.testing.assert_array_equal(observed_mtl["DESI_TARGET"], 2**2 * np.ones(n_obj, dtype=int))
-        np.testing.assert_array_equal(observed_mtl["MWS_TARGET"], np.zeros(n_obj, dtype=int))
-        np.testing.assert_array_equal(observed_mtl["BGS_TARGET"], np.zeros(n_obj, dtype=int))
+        priority = self.targetmask["priorities"]["desi_mask"]["LAE"]["UNOBS"]
+        np.testing.assert_array_equal(observed_mtl["PRIORITY"],
+                                      priority * np.ones(n_obj, dtype=int))
 
-        # TODO no hardcoded - load from targetmask yaml.
-        np.testing.assert_array_equal(observed_mtl["PRIORITY"], 3400 * np.ones(n_obj, dtype=int))
-
-        np.testing.assert_array_equal(observed_mtl["NUMOBS"], np.zeros(n_obj, dtype=int))
-        np.testing.assert_array_equal(observed_mtl["NUMOBS_INIT"], 4 * np.ones(n_obj, dtype=int))
-        np.testing.assert_array_equal(observed_mtl["NUMOBS_MORE"], 4 * np.ones(n_obj, dtype=int))
+        nobs_init = self.targetmask["numobs"]["desi_mask"]["LAE"]
+        np.testing.assert_array_equal(observed_mtl["NUMOBS"],
+                                      np.zeros(n_obj, dtype=int))
+        np.testing.assert_array_equal(observed_mtl["NUMOBS_INIT"],
+                                      nobs_init * np.ones(n_obj, dtype=int))
+        np.testing.assert_array_equal(observed_mtl["NUMOBS_MORE"],
+                                      nobs_init * np.ones(n_obj, dtype=int))
 
         # Subpriority is random but we should ensure that it's between zero and one.
         self.assertTrue(np.all(observed_mtl["SUBPRIORITY"] >= 0))
         self.assertTrue(np.all(observed_mtl["SUBPRIORITY"] <= 1))
 
-    def test_update_mtl_nozcat(self):
+    def test_single_update_mtl_nozcat(self):
         tbl = Table()
         n_obj = 50
         n_update = 20
@@ -58,6 +71,8 @@ class TestMTL(unittest.TestCase):
         # There should be at least 50 + 20 rows (50 original, 20 updated)
         self.assertEqual(len(observed_mtl), n_obj + n_update)
 
+        # Everything is an LAE for now.
+        name = "LAE"
         for tid in tids_to_update:
             # Indices in this loop based on the assumption that update_mtl
             # correctly orders the mtl by targetid and then by timestamp
@@ -74,9 +89,14 @@ class TestMTL(unittest.TestCase):
             # NUMOBS_MORE should be decreased by one relative to previous.
             # Original should match the targetmask yaml.
             self.assertEqual(tid_tbl["NUMOBS_MORE"][0] - 1, tid_tbl["NUMOBS_MORE"][1])
-            self.assertEqual(tid_tbl["NUMOBS_MORE"][0], 4) # TODO not hardcoded
+            self.assertEqual(tid_tbl["NUMOBS_MORE"][0],
+                             self.targetmask["numobs"]["desi_mask"][name]) # TODO derive target type from row.
 
             # The priority both before and after the update should match
             # what's expected in our custom targetmask.
+            self.assertEqual(tid_tbl["PRIORITY"][0],
+                             self.targetmask["priorities"]["desi_mask"][name]["UNOBS"])
+            self.assertEqual(tid_tbl["PRIORITY"][1],
+                             self.targetmask["priorities"]["desi_mask"][name]["MORE_ZGOOD"])
 
             # TODO more tests...
