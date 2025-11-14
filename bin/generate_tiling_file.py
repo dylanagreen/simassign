d1c28b9f6cc6abbd9170581054f928ed84c0d356
dylanagreen <dylanag@uci.edu>
2025-11-14 15:51:09 -0800

    Add first attempt at stripe tiling to generate_tiling_file.

--- bin/generate_tiling_file.py
+++ bin/generate_tiling_file.py
@@ -17,7 +17,7 @@ from desimodel.focalplane import get_tile_radius_deg
 from pathlib import Path
 
 # simassign imports
-from simassign.util import rotate_tiling, check_in_survey_area
+from simassign.util import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--ramax", required=False, type=float, help="maximum RA angle to assign over.")
@@ -30,6 +30,7 @@ parser.add_argument("--trim", required=False, action="store_true", help="trim ti
 parser.add_argument("--starttime", required=False, type=str, default="2025-09-16T00:00:00+00:00", help="starting timestamp for the first tile")
 parser.add_argument("--survey", required=False, type=str, default=None, help="use the survey defined by the boundaries in this file rather than the nominal DESI 2 survey.")
 parser.add_argument("--add_tiledone", required=False, action="store_true", help="add TILEDONE column (for running without a simulated survey).")
+parser.add_argument("--stripes", required=False, action="store_true", help="use stripe tiling instead of DESI-I-like symmetrical tiling.")
 
 group_trim = parser.add_mutually_exclusive_group(required=False)
 group_trim.add_argument("--trim_rad", type=float, help="when trimming, keep only tiles if their center is at least trim_rad/2 away from the survey edge. This convention matches that of the matplotlib path")
@@ -44,45 +45,81 @@ group_pass.add_argument("--npass", type=int, help="number of assignment passes t
 group_pass.add_argument("--ntiles", type=int, help="target number of tiles to achieve, using as many passes as possible to get there.")
 args = parser.parse_args()
 
+
+def add_tile_cols(tiles):
+    tile_tbl = Table(tiles)
+    tile_tbl["PROGRAM"] = "DARK"
+    tile_tbl["OBSCONDITIONS"] = 1
+
+    del tile_tbl["ROW"]
+    del tile_tbl["COL"]
+    return tile_tbl
+
 trim_rad = 0
 if args.trim_rad:
     trim_rad = args.trim_rad
 elif args.trim_scale:
     trim_rad = args.trim_scale * get_tile_radius_deg()
 
-# Load the geometry superset to get the tiling of the entire sky.
-tiles = load_tiles(onlydesi=False, tilesfile="tiles-geometry-superset.ecsv")
-tiles = Table(tiles)
+survey = None
+if args.survey is not None:
+    survey = np.load(args.survey)
 
-# This ensures we only get one tiling of the sky to use as a base.
-zero_pass = tiles["PASS"] == 0
-dark_tile = (tiles["PROGRAM"] == "DARK") | (tiles["PROGRAM"] == "DARK1B")
-base_tiles = tiles[zero_pass & dark_tile]
+if args.stripes:
+    base_tiles = generate_stripe_tiles()
+else:
+    # Load the geometry superset to get the tiling of the entire sky.
+    tiles = load_tiles(onlydesi=False, tilesfile="tiles-geometry-superset.ecsv")
+    tiles = Table(tiles)
 
+    # This ensures we only get one tiling of the sky to use as a base.
+    zero_pass = tiles["PASS"] == 0
+    dark_tile = (tiles["PROGRAM"] == "DARK") | (tiles["PROGRAM"] == "DARK1B")
+    base_tiles = tiles[zero_pass & dark_tile]
+
+in_survey = check_in_survey_area(base_tiles, survey, trim_rad=trim_rad)
+n_base = np.sum(in_survey)
 # Use this to get all tiles that touch the given zone, not just ones that only
 # have a center that falls inside the zone.
 tile_rad =  get_tile_radius_deg()
 margin = tile_rad - 0.4
 
 pass_tilings = []
-if args.twoex: max_pass = (args.npass + 1) // 2
+
+if args.npass:
+    if args.twoex: max_pass = (args.npass + 1) // 2
+    else: max_pass = args.npass + 1
 else:
-    if args.npass:
-        max_pass = args.npass + 1
-    else:
-        max_pass = 1000 # Something big to encompass all tiles necessary.
+    print("n_base", n_base)
+    max_pass = args.ntiles // n_base + 1 # Add one because we iterate from 1 upwards.
+    print("max_pass", max_pass)
+    print(args.ntiles)
+    print("pred tiles", max_pass * n_base)
+    if not args.stripes: max_pass += 1
+
+
+if args.stripes:
+    all_stripes = shift_stripes(max_pass, base_tiles)
 
 cur_tiles = 0
 for i in range(1, max_pass):
     print(f"Generating tiling for pass {i}...")
-    if args.fourex: # Repeat each tiling four times before moving to the next one
-         passnum = (i + 3) // 4
-    elif args.twoex:
-        passnum = (i + 1) // 2
+    if args.stripes:
+        tiles = add_tile_cols(all_stripes[i - 1])
+        tiles["PASS"] = i
+
+        tileids = np.arange(len(tiles)) + i * 10000
+        tiles["TILEID"] = tileids
+
     else:
-         passnum = i
-    print("PASSNUM", passnum)
-    tiles = rotate_tiling(base_tiles, passnum)
+        if args.fourex: # Repeat each tiling four times before moving to the next one
+            passnum = (i + 3) // 4
+        elif args.twoex:
+            passnum = (i + 1) // 2
+        else:
+            passnum = i
+        print("PASSNUM", passnum)
+        tiles = rotate_tiling(base_tiles, passnum)
 
     # IF we're not collapsing but we are doing 4x, give each "pass" a unique
     # tileid, so that we keep all four passes on joins.
@@ -102,9 +139,6 @@ for i in range(1, max_pass):
         tiles["IN_DESI"][not_in_zone] = False
 
     if args.trim:
-        survey = None
-        if args.survey is not None:
-            survey = np.load(args.survey)
         in_survey = check_in_survey_area(tiles, survey, trim_rad=trim_rad)
         tiles["IN_DESI"] = False
         tiles["IN_DESI"][in_survey] = True
