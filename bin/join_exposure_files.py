0accf4b343ec65d306065721849542e03bd6dbc7
dylanagreen <dylanag@uci.edu>
2025-12-10 08:58:43 -0800

    Add fix_tileids to join_exposure_files and make exposures required args.

--- bin/join_exposure_files.py
+++ bin/join_exposure_files.py
@@ -8,14 +8,18 @@ import numpy as np
 from astropy.table import Table, vstack
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--exposures_a", required=True, type=str, help="earlier exposures file, representing the start of the survey.")
-parser.add_argument("--exposures_b", required=True, type=str, help="later exposure file to join to the earlier file.")
+parser.add_argument("exposures_a", type=str, help="earlier exposures file, representing the start of the survey.")
+parser.add_argument("exposures_b", type=str, help="later exposure file to join to the earlier file.")
 parser.add_argument("-o", "--out", required=True, type=str, help="where to save generated tile file.")
-
+parser.add_argument("--fix_pass", required=False, action="store_true", help="fix pass numbers in exposures_b to have unique passes.")
+parser.add_argument("--fix_tileids", required=False, action="store_true", help="fix tileids to be unique across the joined file")
 args = parser.parse_args()
 
 tbl_a = Table.read(args.exposures_a)
 tbl_b = Table.read(args.exposures_b)
+
+print(f"Len A {len(tbl_a)}")
+print(f"Len B {len(tbl_b)}")
 
 # These should be sorted by exposure date but they might not be
 tbl_a.sort("TIMESTAMP")
@@ -36,14 +40,27 @@ tbl_b["TIMESTAMP"] = [x.isoformat() for x in ts]
 ts_ymd = [x.strftime("%Y%m%d") for x in ts]
 tbl_b["TIMESTAMP_YMD"] = ts_ymd
 
-# Need to increase pass numbers and tileids in exposures_b if they comflict with exposures_a.
-max_pass_a = np.unique(tbl_a["PASS"])[-1]
-min_pass_b = np.unique(tbl_b["PASS"])[0]
-if min_pass_b <= max_pass_a:
-    delta_pass = max_pass_a - min_pass_b + 1
-    tbl_b["PASS"] += delta_pass
+# Need to increase pass numbers and tileids in exposures_b if they conflict with exposures_a.
+if args.fix_pass:
+    max_pass_a = np.unique(tbl_a["PASS"])[-1]
+    min_pass_b = np.unique(tbl_b["PASS"])[0]
+    if min_pass_b <= max_pass_a:
+        delta_pass = max_pass_a - min_pass_b + 1
+        tbl_b["PASS"] += delta_pass
 
 joined = vstack([tbl_a, tbl_b])
+
+if args.fix_tileids:
+    print(np.max(joined["PASS"]))
+    for p in range(np.max(joined["PASS"]) + 1):
+        this_pass = joined["PASS"] == p
+        if np.sum(this_pass):
+            tileids = np.arange(np.sum(this_pass)) + p * 10000
+            print(np.sum(this_pass))
+            print(p, np.max(tileids))
+            joined["TILEID"][this_pass] = tileids
+
+    assert len(np.unique(joined["TILEID"])) == len(joined), "Some non unique tileids!"
 
 print(joined)
 print(f"{len(joined)} exposures")
